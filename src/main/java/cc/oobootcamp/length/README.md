bcb7f133bead2763d8cb6e948f9935325233ebdc
Zhe ZHAO <zhezhao@thoughtworks.com>
2019-06-27 13:28:50 +0800

    Refactor

--- src/main/java/cc/oobootcamp/length/README.md
+++ src/main/java/cc/oobootcamp/length/README.md
@@ -1,11 +1,11 @@
-**GIVEN** the inner value of source is larger than target   
+**GIVEN** the source Length longer than target Length     
 **WHEN** compare source to target  
 **THEN** return *LARGER* as result
 ---
-**GIVEN** the inner value of source is smaller than target   
+**GIVEN** the source Length shorter than target Length     
 **WHEN** compare source to target  
 **THEN** return *SMALLER* as result
 ---
-**GIVEN** the inner value of source equals to target   
+**GIVEN** the source Length equals to target Length  
 **WHEN** compare source to target  
 **THEN** return *EQUALS* as result
--- src/test/java/cc/oobootcamp/length/LengthTest.java
+++ src/test/java/cc/oobootcamp/length/LengthTest.java
@@ -9,26 +9,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LengthTest {
 
     @Test
-    public void should_return_LARGER_when_compare_source_to_targer_given_source_is_larger() {
+    public void should_return_LARGER_when_compare_source_to_target_given_source_is_larger() {
         Length sourceLength = new Length(5);
         Length targetLength = new Length(1);
 
-        assertThat(sourceLength.compareTo(targetLength)).isEqualTo(LARGER);
+        int result = sourceLength.compareTo(targetLength);
+
+        assertThat(result).isEqualTo(LARGER);
     }
 
     @Test
-    public void should_return_SMALLER_when_compare_source_to_targer_given_source_is_smaller() {
-        Length lengthOne = new Length(2);
-        Length lengthTwo = new Length(4);
+    public void should_return_SMALLER_when_compare_source_to_target_given_source_is_smaller() {
+        Length sourceLength = new Length(2);
+        Length targetLength = new Length(4);
 
-        assertThat(lengthOne.compareTo(lengthTwo)).isEqualTo(SMALLER);
+        int result = sourceLength.compareTo(targetLength);
+
+        assertThat(result).isEqualTo(SMALLER);
     }
 
     @Test
-    public void should_return_EQUALS_when_compare_source_to_targer_given_value_are_same() {
-        Length lengthOne = new Length(100);
-        Length lengthTwo = new Length(100);
+    public void should_return_EQUALS_when_compare_source_to_target_given_length_are_same() {
+        Length sourceLength = new Length(100);
+        Length targetLength = new Length(100);
 
-        assertThat(lengthOne.compareTo(lengthTwo)).isEqualTo(EQUALS);
+        int result = sourceLength.compareTo(targetLength);
+
+        assertThat(result).isEqualTo(EQUALS);
     }
 }
